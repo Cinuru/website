814773177521a58e19044ecef3048810160684cb
Yassine Sania <yassinesania@gmail.com>
2017-10-11 17:00:04 +0200

    Add logo to homepage

--- src/layouts/Homepage.js
+++ src/layouts/Homepage.js
@@ -3,16 +3,37 @@ import PropTypes from 'proptypes'
 import styled from 'styled-components'
 
 import Container from './Container'
+import { Link } from 'phenomic'
 
 const Boarding = styled.div`
 	background: linear-gradient(to bottom, #2dd7d3, #8c7ddd);
 `
 
+const Logo = styled.span`
+		    padding: 1rem 2rem;
+				display: inline-block;
+				&:hover,
+				&:active,
+				&:focus {
+						transform: scale(1.05);
+				}
+				img {
+						width: auto;
+						height: 2rem;
+				}
+		}
+`
+
 const HomePage = props => (
 	<Container {...props}>
 		<Boarding>
+			<Logo>
+					<Link to="/">
+							<img src="/assets/logo.svg" alt="Cinuru" />
+					</Link>
+			</Logo>
 			<iframe
-				src="http://85.214.78.13:3000/"
+				src="http://localhost:3000/"
 				width="100%"
 				height="660px"
 				allowTransparency="true"
