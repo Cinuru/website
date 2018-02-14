4f15490a366bbd8c121f70a436351f0efe41c6f7
Rui Paulo Ruhrländer <p.ruhrlaender@codeispoetry.de>
2018-02-14 18:03:34 +0100

    Script to generate duplicated pages from index.md

---
+++ generate_urls.js
@@ -0,0 +1,40 @@
+const fs = require('fs-extra');
+
+let pages = [
+	'cinexx_website.md',
+	'hhoefe_website.md',
+	'app.md',
+	'lumos_website.md',
+	'scala_newsletter.md',
+	'scala_website.md',
+	'download_qr.md',
+	'thalia_website.md',
+	'katniss.md',
+	'scala.md',
+	'dude.md',
+	'lumos_newsletter.md',
+	'jena_newsletter.md',
+	'thalia-welcome-6ee843fd.md',
+	'casino_website.md',
+	'umfrage.md',
+	'casino_newsletter.md',
+	'kinoapp.md',
+	'social.md',
+	'kinoapp_qr.md',
+	'testen.md',
+	'thalia_newsletter.md',
+	'jena_website.md',
+	'cinexx_newsletter.md',
+	'leia.md',
+	'app_qr.md',
+	'download.md',
+	'hhoefe_newsletter.md',
+];
+const main = async () => {
+	pages.map(p => {
+		fs.removeSync('./content/' + p);
+		fs.copySync('./content/index.md', './content/' + p);
+	});
+};
+
+main();
